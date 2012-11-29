81fa824a91d07ca8d9bda95673feec8a3cfe4b16
Tom De Vylder <tom@penumbra.be>
2012-11-29 21:26:07 +0100

    Add config file/dir mode

--- manifests/config.pp
+++ manifests/config.pp
@@ -6,11 +6,13 @@ class basemodule::config {
     notify  => Service[$::basemodule::service_name],
     owner   => $::basemodule::config_user,
     group   => $::basemodule::config_group,
+    mode    => $::basemodule::config_file_mode,
   }
 
   file {
     $::basemodule::config_dir:
-      ensure => directory;
+      ensure => directory,
+      mode   => $::basemodule::config_dir_mode;
 
     $::basemodule::log_dir:
       ensure => directory,
