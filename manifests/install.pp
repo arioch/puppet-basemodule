14757b5410cf71b705e41e1426d67f9c6f6ceb98
Tom De Vylder <tom@penumbra.be>
2012-12-14 13:41:43 +0100

    Add package dependencies

--- manifests/install.pp
+++ manifests/install.pp
@@ -4,5 +4,12 @@ class basemodule::install {
   package { $::basemodule::pkg_list:
     ensure => $::basemodule::pkg_ensure,
   }
+
+  if $::basemodule::pkg_deps {
+    package { $::awstats::pkg_deps:
+      ensure  => $::awstats::pkg_ensure,
+      require => $::awstats::pkg_list,
+    }
+  }
 }
 
