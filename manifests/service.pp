8a02b1c6a169a6d34651912527d7d476f28dae57
Tom De Vylder <tom@penumbra.be>
2013-05-25 13:43:10 +0200

    Add missing parameter: hasrestart

--- manifests/service.pp
+++ manifests/service.pp
@@ -3,27 +3,30 @@
 class basemodule::service {
   if $::basemodule::service_dep {
     service { $::basemodule::service_dep:
-      ensure    => $::basemodule::service_ensure,
-      enable    => $::basemodule::service_enable,
-      hasstatus => $::basemodule::service_hasstatus,
-      require   => Class['::basemodule::config'];
+      ensure     => $::basemodule::service_ensure,
+      enable     => $::basemodule::service_enable,
+      hasrestart => $::basemodule::service_hasrestart,
+      hasstatus  => $::basemodule::service_hasstatus,
+      require    => Class['::basemodule::config'];
     }
 
     service { $::basemodule::service_name:
-      ensure    => $::basemodule::service_ensure,
-      enable    => $::basemodule::service_enable,
-      hasstatus => $::basemodule::service_hasstatus,
-      require   => [
+      ensure     => $::basemodule::service_ensure,
+      enable     => $::basemodule::service_enable,
+      hasrestart => $::basemodule::service_hasrestart,
+      hasstatus  => $::basemodule::service_hasstatus,
+      require    => [
         Service['::basemodule::service_dep'],
         Class['::basemodule::config'],
       ];
     }
   } else {
     service { $::basemodule::service_name:
-      ensure    => $::basemodule::service_ensure,
-      enable    => $::basemodule::service_enable,
-      hasstatus => $::basemodule::service_hasstatus,
-      require   => Class['::basemodule::config'];
+      ensure     => $::basemodule::service_ensure,
+      enable     => $::basemodule::service_enable,
+      hasrestart => $::basemodule::service_hasrestart,
+      hasstatus  => $::basemodule::service_hasstatus,
+      require    => Class['::basemodule::config'];
     }
   }
 }
